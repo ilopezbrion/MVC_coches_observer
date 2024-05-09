a16ca99ad97647c799cc8bf87d11f8b4001803be
Damian Nogueiras <damian@danielcastelao.org>
2024-05-09 20:05:05 +0200

    Añadimos clase App para el main inicial

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    /**
+     * Este es el punto inicial de nuestra aplicación
+     * Las tareas iniciales estarían en este método
+     * @param args no usadas
+     */
+    public static void main(String[] args) {
+        // inicializamos la vista
+        View.crearVentana();
+        // debug
+        System.out.println("fin del main");
+    }
+}
